2e808ea3692cf4ad8419d7b9887d628bb448c2ee
derrick <derricknjeru5@gmail.com>
2019-04-23 15:56:27 +0300

    initial commit

---
+++ src/main/StringCalculator.java
@@ -0,0 +1,21 @@
+package main;
+
+public class StringCalculator {
+
+    public static int add(String numbers) {
+        int returnValue = 0;
+        if (numbers.isEmpty()) return returnValue;
+
+        String[] numbersArray = numbers.split(",");
+        /*if (numbersArray.length > 2) {
+            throw new RuntimeException("Up to 2 numbers separated by comma (,) are allowed");
+        } else {*/
+        for (String number : numbersArray) {
+            if (!number.trim().isEmpty()) { // After refactoring
+                returnValue += Integer.parseInt(number);
+            }
+        }
+        //}
+        return returnValue;
+    }
+}
---
+++ src/test/SimpleCalculatorTest.java
@@ -0,0 +1,37 @@
+package test;
+
+import main.StringCalculator;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SimpleCalculatorTest {
+
+    /*@Test(expected = RuntimeException.class)
+    public final void whenMoreThan2NumbersAreUsedThenExceptionIsThrown() {
+        StringCalculator.add("1,2,3");
+    }*/
+
+    @Test
+    public final void whenEmptyStringIsUsedThenReturnValueIs0() {
+        Assert.assertEquals(0, StringCalculator.add(""));
+    }
+
+
+    @Test
+    public final void whenOneNumberIsUsedThenReturnValueIsThatSameNumber() {
+        Assert.assertEquals(3, StringCalculator.add("3"));
+    }
+
+    @Test
+    public final void whenTwoNumbersAreUsedThenReturnValueIsTheirSum() {
+        Assert.assertEquals(3 + 6, StringCalculator.add("3,6"));
+    }
+
+
+    @Test
+    public final void whenAnyNumberOfNumbersIsUsedThenReturnValuesAreTheirSums() {
+        Assert.assertEquals(3 + 6 + 15 + 18 + 46 + 33, StringCalculator.add("3,6,15,18,46,33"));
+    }
+
+
+}
